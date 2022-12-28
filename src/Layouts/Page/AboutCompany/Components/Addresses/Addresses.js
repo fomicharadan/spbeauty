4367945e6ba8634b16870f074730861e3cac7338
fomicharadan <112316733+fomicharadan@users.noreply.github.com>
2022-12-28 14:15:57 +0300

    Add files via upload

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,15 +1,16 @@
-import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
+        <div className='d-inline-flex flex-column col justify-content-between p-4 border rounded-3'>
+            <div className='d-flex flex-column gap-4'>
+                <div className='title'>Адреса заведений</div>
+                <div className='d-inline-flex flex-column gap-2'>
+                    <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+                    <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>                
+                </div>
+            </div>            
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
